2dfbad349ab4e58ba197a0b2a4a5a2f7232bb44a
1192704 <abhinavgudipati@users.noreply.github.com>
2026-08-07 18:54:58 +0530

    fix: nudge photo crop to top

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
             <img
               src="/photo2.jpg.jpg"
               alt="Abhinav Gudipati"
-              style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "55% 5%", transform: "scale(1.6)", transformOrigin: "55% 5%" }}
+              style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "55% 0%", transform: "scale(1.6)", transformOrigin: "55% 0%" }}
             />
           </div>
         </div>
